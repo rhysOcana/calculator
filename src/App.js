5216b01b98368687cb432c9579774b0c92539112
Louis-Navidad <johnlouis.alquisalas2022@mauihigh.org>
2023-01-30 15:46:55 -1000

    calculatore with buttons and functions

--- src/App.js
+++ src/App.js
@@ -1,24 +1,96 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [num1, setNum1] = useState(0);
+  const [num2 , setNum2] = useState(0);
+  const [result, setResult] = useState("");
+  const [op, setOp] = useState("");
+  const [check, setCheck] = useState(false);
+
+  const buttonClick = (e) =>{
+   if(check === false){
+    setNum1(num1 + e.target.textContent);
+   } 
+   
+   else{
+    setNum2(num2 + e.target.textContent);
+   }
+   setResult(result + e.target.textContent);
+  }
+
+  const opBtn = (e) => {
+   setOp(e.target.textContent);
+   setResult(result + e.target.textContent);
+   setCheck(true);
+  }
+
+  function handleClear(){
+    setNum1(0);
+    setNum2(0);
+    setOp("");
+    setResult("");
+    setCheck(false);
+  }
+
+  function handleResultClick(){
+    console.log(num1);
+    console.log(num2);
+    switch(op){
+      case "+":
+        console.log("add");
+        setResult(parseInt(num1) + parseInt(num2));
+        break;
+
+      case "-":
+        console.log("substract");
+        setResult(parseInt(num1) - parseInt(num2));
+        break;
+      
+      case "*":
+        console.log("multipy");
+        setResult(parseInt(num1) * parseInt(num2));
+        break;
+
+      case "/":
+        console.log("divide");
+        setResult(parseInt(num1) / parseInt(num2) );
+        break;
+
+      default:  
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div>
+    <div>
+      <input value={result}></input>
     </div>
+    <div>
+      <button onClick={buttonClick}>0</button>
+      <button onClick={buttonClick}>1</button>
+      <button onClick={buttonClick}>2</button>
+      <button onClick={opBtn}>+</button>
+    </div>
+    <div>
+    <button onClick={buttonClick}>3</button>
+    <button onClick={buttonClick}>4</button>
+    <button onClick={buttonClick}>5</button>
+    <button onClick={opBtn}>-</button>
+    </div>
+    <div>
+    <button onClick={buttonClick}>6</button>
+    <button onClick={buttonClick}>7</button>
+    <button onClick={buttonClick}>8</button>
+    <button onClick={opBtn}>*</button>
+    </div>
+    <div>
+    <button onClick={buttonClick}>9</button>
+    <button onClick={opBtn}>/</button>
+    <button onClick={handleResultClick}>=</button>
+    <button onClick={handleClear}>AC</button>
+    </div>
+  </div>  
   );
 }
 
